d28b41af62dcb7484cea8eed3ad4dccf33e674db
igorjuk22 <igor.juk.lima@escola.pr.gov.br>
2026-06-18 08:59:09 -0300

    botao fonte

---
+++ script.js
@@ -0,0 +1,24 @@
+const botao = document.querySelector("button")
+botao.addEventListener('click', curtir)
+
+function curtir() {
+    let curtidas = document.querySelector("span")
+    curtidas.textContent++;
+}
+
+
+// BOTÃO DO TAMANHO
+const tamanho = document.getElementById("tamanho")
+tamanho.addEventListener("click", aumentarFonte)
+let fonte = 16;
+
+//FUNÇÃO
+function aumentarFonte(){
+    fonte++;
+    document.body.style.fontSize = fonte + "px";
+}
+
+// Botão do Contraste
+const bcontraste = document.
+
+// FUNÇÃO DO CONTRASTE
